fae817b5ed1418dd4da27a2e76db932ce28782d6
Eric Wan <eric.wan@komodoopenlab.com>
2013-03-11 12:18:24 -0400

    Created classes for HUD.

---
+++ highlighter/src/ca/idrc/tecla/TeclaHUD.java
@@ -0,0 +1,13 @@
+package ca.idrc.tecla;
+
+import android.content.Context;
+import android.view.View;
+
+public class TeclaHUD extends View {
+
+	public TeclaHUD(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ highlighter/src/ca/idrc/tecla/TeclaHUDAsset.java
@@ -0,0 +1,5 @@
+package ca.idrc.tecla;
+
+public class TeclaHUDAsset {
+
+}
