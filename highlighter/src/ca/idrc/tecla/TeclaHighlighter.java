e283e116121398e5d430ad952b3a5246019280f0
Eric Wan <eric.wan@komodoopenlab.com>
2013-03-16 15:22:30 -0400

    Cleared highlights after selection.

--- highlighter/src/ca/idrc/tecla/TeclaHighlighter.java
+++ highlighter/src/ca/idrc/tecla/TeclaHighlighter.java
@@ -48,6 +48,8 @@ public class TeclaHighlighter extends SimpleOverlay {
 	public static void clearHighlight() {
         sInstance.mOuterBounds.clear();
         sInstance.mInnerBounds.clear();
+        sInstance.mOuterBounds.postInvalidate();
+        sInstance.mInnerBounds.postInvalidate();
 	}
 	
     public static void removeInvalidNodes() {
