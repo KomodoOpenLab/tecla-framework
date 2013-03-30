77895020acde9aa1035b77bb3b84588d93a5b8fa
Eric Wan <eric.wan@komodoopenlab.com>
2013-03-30 04:34:26 -0400

    Updated graphics on scan.

--- java/src/ca/idrc/tecla/TeclaHUDController.java
+++ java/src/ca/idrc/tecla/TeclaHUDController.java
@@ -1,18 +1,24 @@
 package ca.idrc.tecla;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.ImageView;
 
 public class TeclaHUDController extends SimpleOverlay {
 
 	private static TeclaHUDController sInstance;
 
+	private ArrayList<ImageView> mHUDPad;
+	private ArrayList<ImageView> mHUDPadHighlight;
+	private ArrayList<ImageView> mHUDSymbol;
+	private ArrayList<ImageView> mHUDSymbolHighlight;
+	
 	protected final static long SCAN_PERIOD = 1500;
 	private byte mState;
 	protected final static byte TOTAL_STATES = 5;
@@ -37,8 +43,48 @@ public class TeclaHUDController extends SimpleOverlay {
 
 		getRootView().setOnLongClickListener(mOverlayLongClickListener);
 		getRootView().setOnClickListener(mOverlayClickListener);
+		
+		mHUDPad = new ArrayList<ImageView>();
+		mHUDPad.add((ImageView)findViewById(R.id.imageView_border_up));
+		mHUDPad.add((ImageView)findViewById(R.id.imageView_border_upperright));
+		mHUDPad.add((ImageView)findViewById(R.id.imageView_border_right));
+		mHUDPad.add((ImageView)findViewById(R.id.imageView_border_lowerright));
+		mHUDPad.add((ImageView)findViewById(R.id.imageView_border_down));
+		mHUDPad.add((ImageView)findViewById(R.id.imageView_border_lowerleft));
+		mHUDPad.add((ImageView)findViewById(R.id.imageView_border_left));
+		mHUDPad.add((ImageView)findViewById(R.id.imageView_border_upperleft));
 
-        mState = STATE_UP;
+		mHUDPadHighlight = new ArrayList<ImageView>();
+		mHUDPadHighlight.add((ImageView)findViewById(R.id.imageView_highlight_up));
+		mHUDPadHighlight.add((ImageView)findViewById(R.id.imageView_highlight_upperright));
+		mHUDPadHighlight.add((ImageView)findViewById(R.id.imageView_highlight_right));
+		mHUDPadHighlight.add((ImageView)findViewById(R.id.imageView_highlight_lowerright));
+		mHUDPadHighlight.add((ImageView)findViewById(R.id.imageView_highlight_down));
+		mHUDPadHighlight.add((ImageView)findViewById(R.id.imageView_highlight_lowerleft));
+		mHUDPadHighlight.add((ImageView)findViewById(R.id.imageView_highlight_left));
+		mHUDPadHighlight.add((ImageView)findViewById(R.id.imageView_highlight_upperleft));
+		
+		mHUDSymbol = new ArrayList<ImageView>();
+		mHUDSymbol.add((ImageView)findViewById(R.id.imageView_border_uparrow));
+		mHUDSymbol.add((ImageView)findViewById(R.id.imageView_border_select));
+		mHUDSymbol.add((ImageView)findViewById(R.id.imageView_border_rightarrow));
+		mHUDSymbol.add((ImageView)findViewById(R.id.imageView_border_forward));
+		mHUDSymbol.add((ImageView)findViewById(R.id.imageView_border_downarrow));
+		mHUDSymbol.add((ImageView)findViewById(R.id.imageView_border_back));
+		mHUDSymbol.add((ImageView)findViewById(R.id.imageView_border_leftarrow));
+		mHUDSymbol.add((ImageView)findViewById(R.id.imageView_border_home));
+				
+		mHUDSymbolHighlight = new ArrayList<ImageView>();
+		mHUDSymbolHighlight.add((ImageView)findViewById(R.id.imageView_highlight_uparrow));
+		mHUDSymbolHighlight.add((ImageView)findViewById(R.id.imageView_highlight_select));
+		mHUDSymbolHighlight.add((ImageView)findViewById(R.id.imageView_highlight_rightarrow));
+		mHUDSymbolHighlight.add((ImageView)findViewById(R.id.imageView_highlight_forward));
+		mHUDSymbolHighlight.add((ImageView)findViewById(R.id.imageView_highlight_downarrow));
+		mHUDSymbolHighlight.add((ImageView)findViewById(R.id.imageView_highlight_back));
+		mHUDSymbolHighlight.add((ImageView)findViewById(R.id.imageView_highlight_leftarrow));
+		mHUDSymbolHighlight.add((ImageView)findViewById(R.id.imageView_highlight_home));
+		
+        mState = STATE_HOME;
         mAutoScanHandler.sleep(1000);
 	}
 
@@ -76,17 +122,22 @@ public class TeclaHUDController extends SimpleOverlay {
 		case(STATE_UP):
 			TeclaAccessibilityService.selectNode(TeclaAccessibilityService.DIRECTION_UP);
 			break; 
+		case(STATE_SELECT):
+			break; 
 		case(STATE_RIGHT):
 			TeclaAccessibilityService.selectNode(TeclaAccessibilityService.DIRECTION_RIGHT);
+			break; 
+		case(STATE_FORWARD):
 			break; 
 		case(STATE_DOWN):
 			TeclaAccessibilityService.selectNode(TeclaAccessibilityService.DIRECTION_DOWN);
 			break; 
+		case(STATE_BACK):
+			break; 
 		case(STATE_LEFT):
 			TeclaAccessibilityService.selectNode(TeclaAccessibilityService.DIRECTION_LEFT);
 			break; 
-		case(STATE_SELECT):
-			TeclaAccessibilityService.clickActiveNode();
+		case(STATE_HOME):
 			break; 
 		default: 
 			break; 
@@ -96,27 +147,36 @@ public class TeclaHUDController extends SimpleOverlay {
 	protected void scanForward() {
 		++mState;
 		mState %= TOTAL_STATES;
-		
+
 		// update HUD graphics 
-		switch (mState) {
-		case(STATE_UP):
-			break; 
-		case(STATE_SELECT):
-			break; 
-		case(STATE_RIGHT):
-			break; 
-		case(STATE_FORWARD):
-			break; 
-		case(STATE_DOWN):
-			break; 
-		case(STATE_BACK):
-			break; 
-		case(STATE_LEFT):
-			break; 
-		case(STATE_HOME):
-			break; 
-		default: 
-			break; 
+		
+		ImageView img;
+		
+		mHUDPad.add((ImageView)mHUDPad.get(0));
+		mHUDPad.remove(0);
+
+		img = (ImageView)mHUDPadHighlight.get(mHUDPadHighlight.size()-1);
+		img.setVisibility(View.INVISIBLE);
+		img = (ImageView)mHUDPadHighlight.get(0);
+		img.setVisibility(View.VISIBLE);
+		mHUDPadHighlight.remove(0);
+		mHUDPadHighlight.add(img);
+		
+		mHUDSymbol.add((ImageView)mHUDSymbol.get(0));
+		mHUDSymbol.remove(0);
+
+		img = (ImageView)mHUDSymbolHighlight.get(mHUDSymbolHighlight.size()-1);
+		img.setVisibility(View.INVISIBLE);
+		img = (ImageView)mHUDSymbolHighlight.get(0);
+		img.setVisibility(View.VISIBLE);
+		mHUDSymbolHighlight.remove(0);
+		mHUDSymbolHighlight.add(img);		
+		
+		for(int i=0; i<mHUDPad.size()-1; ++i) {
+			img = (ImageView)mHUDPad.get(i);
+			img.setAlpha(1.0f/(mHUDPad.size()-1)*i);
+			img = (ImageView)mHUDSymbol.get(i);
+			img.setAlpha(1.0f/(mHUDSymbol.size()-1)*i);
 		}
 		
 		mAutoScanHandler.sleep(SCAN_PERIOD);
