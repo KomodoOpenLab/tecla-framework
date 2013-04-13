02dd93f9240133013a0a939e2668a6e73c02eb9f
Eric Wan <eric.wan@komodoopenlab.com>
2013-04-13 00:23:58 -0400

    Plugged single-switch interface into TeclaIME (interface only shows when keyboard shows).

--- ime-scan/src/ca/idrc/tecla/imescan/TeclaIME.java
+++ ime-scan/src/ca/idrc/tecla/imescan/TeclaIME.java
@@ -1,5 +1,6 @@
 package ca.idrc.tecla.imescan;
 
+import ca.idrc.tecla.touchinterface.SingleSwitchInterface;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.view.View;
@@ -8,15 +9,35 @@ import android.view.inputmethod.EditorInfo;
 public class TeclaIME extends InputMethodService {
 
 	private static TeclaIME sInstance;
+	private SingleSwitchInterface mSSI = null;
 
 	@Override
 	public void onCreate() {
-		super.onCreate();
+		super.onCreate();		
 		sInstance = this;
 	}
 	
 	public static TeclaIME getInstance() {
 		return sInstance;
 	}
+
+	@Override
+	public void onStartInputView(EditorInfo info, boolean restarting) {
+		if(mSSI == null) {
+			mSSI = new SingleSwitchInterface(this);	
+			mSSI.show();
+		}		
+		super.onStartInputView(info, restarting);
+	}
+
+	@Override
+	public void onFinishInputView(boolean finishingInput) {
+		if(mSSI != null) {
+			mSSI.hide();
+			mSSI = null;
+		}
+		super.onFinishInputView(finishingInput);
+	}
+	
 	
 }
