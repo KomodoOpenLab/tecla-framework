ba229bf676435d8a237214911dc35deaf5a4f98e
Eric Wan <eric.wan@komodoopenlab.com>
2013-04-26 02:31:42 -0400

    Completed implementation of word prediction scanning.

--- package/tecla/keyboard/WordPredictionAdapter.java
+++ package/tecla/keyboard/WordPredictionAdapter.java
@@ -1,48 +1,101 @@
 package com.android.tecla.keyboard;
 
+import android.graphics.Color;
+import android.view.View;
 import android.view.ViewGroup;
 
 public class WordPredictionAdapter {
 
-	private static int[] sSuggestionsViewIndices = {0,2,4};
+	private static final int[] SUGGESTIONSVIEWINDICES = {0,2,4};
+	private static final int BACKGROUND_HIGHLIGHT_COLOR = Color.BLUE;
+	private static final int BACKGROUND_NORMAL_COLOR = Color.LTGRAY;
+	
 	private static ViewGroup sSuggestionsViewGroup;
 	
 	public static void setSuggestionsViewGroup(ViewGroup vg) {
 		sSuggestionsViewGroup = vg;
 	}
 	
-	public static void scanNext() {
-		
-	}
-	
-	public static void scanPrevious() {
-		
-	}
-	
 	public static void selectHighlighted() {
-		
+		WordPredictionStates.click();
+	}
+
+	public static void highlightNext() {
+		WordPredictionStates.scanNext();
+	}
+
+	public static void highlightPrevious() {
+		WordPredictionStates.scanPrevious();
+	}
+	
+	private static void highlightSuggestion(int index, boolean highlight) {
+		if(index < 0 || index >= SUGGESTIONSVIEWINDICES.length) return;
+		View view = sSuggestionsViewGroup.getChildAt(SUGGESTIONSVIEWINDICES[index]);
+		if(highlight) {
+			view.setBackgroundColor(BACKGROUND_HIGHLIGHT_COLOR);
+		} else {
+			view.setBackgroundColor(BACKGROUND_NORMAL_COLOR);
+		}
 	}
 	
 	private static class WordPredictionStates {
-		private static final int WPSCAN_NONE = 0x7777;
+		private static final int WPSCAN_NONE = 0x6666;
+		private static final int WPSCAN_HIGHLIGHTED = 0x7777;
 		private static final int WPSCAN_SUGGESTIONS = 0x8888;
 		private static final int WPSCAN_MORESUGGESTIONS = 0x9999;
 		private static final int WPSCAN_CLICK = 0xaaaa;
 		private static int sState = WPSCAN_NONE;
 		
-		private static int sCurrentIndex;
+		private static int sCurrentIndex = -1;
 		
 		private static void reset() {
+			sState = WPSCAN_NONE;
 			
+		}
+		
+		private static void scanNext() {
+			switch(sState) {
+			case(WPSCAN_HIGHLIGHTED):		highlightSuggestion(0, false);
+											highlightSuggestion(1, false);
+											highlightSuggestion(2, false);
+											sState = WPSCAN_NONE;
+											break;
+			case(WPSCAN_SUGGESTIONS):		highlightSuggestion(sCurrentIndex++, false);
+											sCurrentIndex %= SUGGESTIONSVIEWINDICES.length + 1;
+											highlightSuggestion(sCurrentIndex, true);
+											break;
+			default:						break;
+			}
+		}
+		
+		private static void scanPrevious() {
+			switch(sState) {
+			case(WPSCAN_HIGHLIGHTED):		highlightSuggestion(0, false);
+											highlightSuggestion(1, false);
+											highlightSuggestion(2, false);
+											sState = WPSCAN_NONE;
+			case(WPSCAN_SUGGESTIONS):		highlightSuggestion(sCurrentIndex--, false);
+											if (sCurrentIndex < 0)
+												sCurrentIndex = SUGGESTIONSVIEWINDICES.length;
+											highlightSuggestion(sCurrentIndex, true);
+			default:						break;
+			}
 		}
 		
 		private static void click() {
 			switch(sState) {
-			case(WPSCAN_NONE):				sState = WPSCAN_SUGGESTIONS;
+			case(WPSCAN_NONE):				break;
+			case(WPSCAN_HIGHLIGHTED):		highlightSuggestion(0, false);
+											highlightSuggestion(1, false);
+											highlightSuggestion(2, false);
+											sState = WPSCAN_SUGGESTIONS;
+											sCurrentIndex = 0;
+											highlightSuggestion(sCurrentIndex, true);
 											break;
-			case(WPSCAN_SUGGESTIONS):		break;
-			case(WPSCAN_MORESUGGESTIONS):	break;
-			case(WPSCAN_CLICK):				break;
+			case(WPSCAN_SUGGESTIONS):		highlightSuggestion(sCurrentIndex, false);
+											sState = WPSCAN_NONE;
+											// TODO: choose word
+											break;
 			default:						break;
 			}
 		}
