fb087a387e7231b540ecddb34fa78c5a4c6472d5
Eric Wan <eric.wan@komodoopenlab.com>
2013-05-09 14:23:27 -0400

    Added preference screen.

--- java/src/ca/idrc/tecla/framework/SettingsActivity.java
+++ java/src/ca/idrc/tecla/framework/SettingsActivity.java
@@ -1,7 +1,15 @@
 package ca.idrc.tecla.framework;
 
+import ca.idrc.tecla.R;
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.addPreferencesFromResource(R.xml.tecla_prefs);
+	}
 
 }
