56227cb3bd530dbe3e0944bf8136b2a373004e25
Eric Wan <eric.wan@komodoopenlab.com>
2013-06-28 22:09:32 -0400

    Renamed IME adapters.

---
+++ package/tecla/addon/LatinIMEAdapter.java
@@ -0,0 +1,55 @@
+package com.android.tecla.addon;
+
+import android.os.Handler;
+import android.os.Message;
+
+import com.android.inputmethod.keyboard.Key;
+import com.android.inputmethod.keyboard.Keyboard;
+import com.android.inputmethod.keyboard.KeyboardView;
+
+public class LatinIMEAdapter implements TeclaIMEAdapter {
+
+	private static final String CLASS_TAG = "LatinIMEAdapter";
+	
+	@Override
+	public void scanDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scanLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scanRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scanUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scanNext() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scanPrevious() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean setKeyboardView(KeyboardView kbv) {
+		return false;
+	}
+
+}
---
+++ package/tecla/addon/TeclaIMEAdapter.java
@@ -0,0 +1,17 @@
+package com.android.tecla.addon;
+
+import com.android.inputmethod.keyboard.KeyboardView;
+
+public interface TeclaIMEAdapter {
+
+	public void scanDown();
+	public void scanLeft();
+	public void scanRight();
+	public void scanUp();
+	
+	public void scanNext();
+	public void scanPrevious();
+	
+	public boolean setKeyboardView(KeyboardView kbv);
+	
+}
