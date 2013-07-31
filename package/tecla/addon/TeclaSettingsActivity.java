b49bedbb4ee3241f8ed89249faeb062e87483a33
Kelvin Mak <kelvin.mak@ryerson.ca>
2013-07-31 14:54:54 -0400

    Code refactor. Receiver to get bluetooth now in the TeclaShieldManager. Will use ShieldActionListener interface to retry connection to shield upon activation of bluetooth.
    Fixed #349

--- package/tecla/addon/TeclaSettingsActivity.java
+++ package/tecla/addon/TeclaSettingsActivity.java
@@ -148,8 +148,6 @@ public class TeclaSettingsActivity extends PreferenceActivity
 			if (newValue.toString().equals("true")) {
 				mConnectionCancelled = false;
 				if (!mTeclaShieldManager.getBluetoothAdapter().isEnabled()) {
-					registerReceiver(btReceiver, new IntentFilter(
-							BluetoothAdapter.ACTION_STATE_CHANGED));
 					startActivity(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE));
 				}else{				
 					if(!mTeclaShieldManager.discoverShield())
@@ -373,18 +371,12 @@ public class TeclaSettingsActivity extends PreferenceActivity
 		dismissDialog();
 	}
 
-	BroadcastReceiver btReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			// TODO Auto-generated method stub
-			int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1);
-			if (state == BluetoothAdapter.STATE_ON){
-				if(!mTeclaShieldManager.discoverShield())
-					mPrefConnectToShield.setChecked(false);
-				else
-					showDiscoveryDialog();
-			}
-		}
-	};
+	@Override
+	public void onBluetoothActivation() {
+		if(!mTeclaShieldManager.discoverShield())
+			mPrefConnectToShield.setChecked(false);
+		else
+			showDiscoveryDialog();
+	}
 
 }
